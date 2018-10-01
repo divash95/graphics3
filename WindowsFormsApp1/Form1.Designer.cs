a7c8a8ce214c1df6be0cf174a924bb1c202ab462
divash95 <divash95@gmail.com>
2018-10-01 23:04:47 +0300

    'init'

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(620, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(648, 111);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Карандаш";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(648, 134);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Заливка";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(648, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Новый";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(648, 157);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(124, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.Text = "Заливка картинкой";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(648, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Выделить границу";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            newImg();
+        }
+
+        private Pen left = new Pen(Color.Black);
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            newImg();
+        }
+
+        private void newImg()
+        {
+            Bitmap im = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(im))
+                g.Clear(Color.White);
+
+            if (pictureBox1.Image != null)
+                pictureBox1.Image.Dispose();
+            pictureBox1.Image = im;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                left.Color = button1.BackColor = colorDialog1.Color;
+        }
+
+        private Point start;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (radioButton2.Checked)
+                filling(e.X, e.Y);
+            else if (radioButton3.Checked)
+                fillingImg(e.X, e.Y);
+            else
+                start = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                if (radioButton1.Checked)
+                    pencil(sender, e);
+        }
+
+        private void pencil(object sender, MouseEventArgs e)
+        {
+            Bitmap im = new Bitmap(pictureBox1.Image);
+            Graphics g = Graphics.FromImage(im);
+            g.DrawLine(left, start, e.Location);
+            start = e.Location;
+            pictureBox1.Image = im;
+        }
+
+        private void filling(int x, int y)
+        {
+            Bitmap im = new Bitmap(pictureBox1.Image);
+            Color cp = im.GetPixel(x, y);
+            fill(im, cp, left, x, y);
+            pictureBox1.Image = im;
+        }
+
+        private void fillingImg(int x, int y)
+        {
+            Bitmap fillImg = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            openFileDialog1.Filter = "ALL Image|*.png;*.bmp;*.jpg;*.gif";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                fillImg = new Bitmap(openFileDialog1.FileName);
+
+            Bitmap im = new Bitmap(pictureBox1.Image);
+            Color cp = im.GetPixel(x, y);
+            fillFromImg(im, cp, fillImg, x, y);
+            pictureBox1.Image = im;
+        }
+
+        private void fillFromImg(Bitmap im, Color col, Bitmap fillImg, int x, int y)
+        {
+            if ((x < 0) || (y < 0) || (x >= im.Width) || (y >= im.Height))
+                return;
+            Color cur = im.GetPixel(x, y);
+            Color icol = fillImg.GetPixel(x % fillImg.Width, y % fillImg.Height);
+            if ((!cur.Equals(col)) || (cur.Equals(icol)))
+                return;
+            int xL, xR;
+            xL = xR = x;
+            while ((xL > 0) && (col.Equals(cur)))
+            {
+                xL--;
+                cur = im.GetPixel(xL, y);
+            }
+            cur = col;
+            while ((xR < im.Width - 1) && (cur.Equals(col)))
+            {
+                xR++;
+                cur = im.GetPixel(xR, y);
+            }
+            if (xL != 0)
+                xL++;
+            if (xR != im.Width - 1)
+                xR--;
+            for (int i = xL; i <= xR; i++)
+            {
+                icol = fillImg.GetPixel(i % fillImg.Width, y % fillImg.Height);
+                im.SetPixel(i, y, icol);
+            }
+
+            for (int i = xL; i <= xR; i++)
+                fillFromImg(im, col, fillImg, i, y + 1);
+
+            for (int i = xL; i <= xR; i++)
+                fillFromImg(im, col, fillImg, i, y - 1);
+        }
+
+        private void fill(Bitmap im, Color col, Pen p, int x, int y)
+        {
+            if ((x < 0) || (y < 0) || (x >= im.Width) || (y >= im.Height))
+                return;
+            Color cur = im.GetPixel(x, y);
+            if ((!cur.Equals(col)) || (cur.Equals(p.Color)))
+                return;
+            int xL, xR;
+            xL = xR = x;
+            while  ((xL > 0) && (col.Equals(cur)))
+            {
+                xL--;
+                cur = im.GetPixel(xL, y);
+            }
+            cur = col;
+            while ((xR < im.Width - 1) && (cur.Equals(col)))
+            {
+                xR++;
+                cur = im.GetPixel(xR, y);
+            }
+            if (xL != 0)
+                xL++;
+            if (xR != im.Width - 1)
+                xR--;
+            Graphics g = Graphics.FromImage(im);
+            g.DrawLine(p, xL, y, xR, y);
+
+            for (int i = xL; i <= xR; i++)
+                fill(im, col, p, i, y + 1);
+
+            for (int i = xL; i <= xR; i++)
+                fill(im, col, p, i, y - 1);
+        }
+
+        private Bitmap img;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            img = new Bitmap(pictureBox1.Image);
+            List<Point> points = findContour();
+            foreach (Point p in points)
+                img.SetPixel(p.X, p.Y, Color.Red);
+            pictureBox1.Image = img;
+        }
+
+        private int dir;
+        private List<Point> findContour()
+        {
+            List<Point> points = new List<Point>();
+            Point startP = findRightTop(img.GetPixel(img.Width - 1, 0));
+            if (startP.X == 0 && startP.Y == 0)
+                return points;
+            points.Add(startP);
+            dir = 6;
+
+            Color col = img.GetPixel(startP.X,  startP.Y);
+            Point nextP = nextPoint(startP, col);
+            points.Add(nextP);
+            while (nextP != startP)
+            {
+                nextP = nextPoint(nextP, col);
+                points.Add(nextP);
+            }
+            return points;
+        }
+
+        private Point findRightTop(Color col)
+        {
+            for (int i = img.Width - 1; i >= 0; i--)
+                for (int j = 0; j < img.Height; j++)
+                    if (col != img.GetPixel(i, j))
+                        return new Point(i, j);
+            return new Point(0, 0);
+        }
+
+        private Point nextPoint(Point curP, Color col)
+        {
+            int startD = (dir + 2) % 8;
+            Point nextP = nextStep(curP, startD);
+            int newD = startD;
+            while(img.GetPixel(nextP.X,nextP.Y) != col)
+            {
+                if (newD < 0)
+                    newD += 8;
+                nextP = nextStep(curP, newD);
+                newD--;
+            }
+            dir = newD;
+            return nextP;
+        }
+        private Point nextStep(Point curP, int d)
+        {
+            switch (d)
+            {
+                case 0: return new Point(curP.X + 1, curP.Y);
+                case 1: return new Point(curP.X + 1, curP.Y - 1);
+                case 2: return new Point(curP.X, curP.Y - 1);
+                case 3: return new Point(curP.X - 1, curP.Y - 1);
+                case 4: return new Point(curP.X - 1, curP.Y);
+                case 5: return new Point(curP.X - 1, curP.Y + 1);
+                case 6: return new Point(curP.X, curP.Y + 1);
+                case 7: return new Point(curP.X + 1, curP.Y + 1);
+                default: return curP;
+            }
+        }
+      
+    }
+}
